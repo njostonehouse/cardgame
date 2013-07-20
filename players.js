9de1bf5c51752ce5cf725e3def3da140ac4e1ec0
Carl Pacey <carl.pacey@gmail.com>
2013-07-19 21:31:14 -0400

    Minor cleanup.

--- players.js
+++ players.js
@@ -1,18 +1,16 @@
 var _ = require('underscore')
 
-var players = {
-	list: [
+var list = exports.list = [
 		{ id: 0, name: "Carl", cardIds: [ 0, 1 ], state: { selectedCard: null, position: 3 } },
 		{ id: 1, name: "Noel", cardIds: [ 1, 2 ], state: { selectedCard: null, position: 2 } },
 		{ id: 2, name: "Sean", cardIds: [ 2, 3 ], state: { selectedCard: null, position: 1 } },
 		{ id: 3, name: "Mike", cardIds: [ 3, 4 ], state: { selectedCard: null, position: 0 } },
-	],
-	findById: function( id ) {
-		return _.find( players.list, function(player) { return player.id == id } )
-	},
-	unselectCards: function() {
-		_.each( players.list, function(player) { player.state.selectedCard = null } )
+	]
+	
+var findById = exports.findById = function( id ) {
+		return _.find( list, function(player) { return player.id == id } )
 	}
-}
 
-module.exports = players
+var unselectCards = exports.unselectCards = function() {
+	_.each( list, function(player) { player.state.selectedCard = null } )
+}
